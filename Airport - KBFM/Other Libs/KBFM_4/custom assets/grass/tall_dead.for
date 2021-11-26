d635e523a4702109bdcfaed203b11d282662d637
pauloibm <pauloibm@gmail.com>
2021-11-26 14:05:31 -0300

    Update1.2

---
+++ Airport - KBFM/Other Libs/KBFM_4/custom assets/grass/tall_dead.for
@@ -0,0 +1,18 @@
+A
+800
+FOREST
+
+TEXTURE grass.png
+NO_SHADOW
+
+SCALE_X 4096				
+SCALE_Y 4096
+
+SPACING 1.95 1.95
+RANDOM 0.6 0.6
+#	low-left	tex size	center	percent	--height--
+# tree	s	t	w	y	offset	occur	min	max	quads	type	name
+#------------------------------------------------------------------------------------------
+TREE	0	1787	1232	769		630		100		0.8 	1.1 	2 	0
+
+SKIP_SURFACE water
